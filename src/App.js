746ba9c6a06af140f23ad5f336c350ef2d0e5afe
mamathachowdarym <mamathachowdarym@gmail.com>
2023-04-28 14:03:21 +0530

    added experience and education

--- src/App.js
+++ src/App.js
@@ -9,18 +9,47 @@ function App() {
         <section className='resume-section p-3 p-lg-5' id='about'>
           <div className='auto'>
             <h1 className='mb-0'>Pallavi Ranganath</h1>
-            <p className='subheading '>pallavi.masineni@gmail.com</p> 
-            <p className='aboutme'>I am a creative designer and developer, who aims to work with small businesses and marginalized communities to bring their passions to life. I offer both design and development services of web applications or websites!</p> 
+            <div className='subheading '>pallavi.masineni@gmail.com</div> 
+            <p className='about'>I am a creative designer and developer, who aims to work with small businesses and marginalized communities to bring their passions to life. I offer both design and development services of web applications or websites!</p> 
             <ul className='list-inline list-social-icons'>
               <li className='list-inline-item'>
-                <a href='#'>
+                <a href='/'>
                   < BsGithub className='fa fa-stack' />
                 </a>
-                <a href='#'>
+                <a href='/'>
                    < BsLinkedin className='fa fa-stack' />
                 </a>
               </li>
             </ul>
+          </div>
+        </section>
+        <section className='resume-section p-3 p-lg-5' id='experience'>
+          <div className='auto'>
+            <h2 className='mb-5'>Experience</h2>
+            <div className='resume-item '>
+              <div className='resume-content mr-auto'>
+                <h3 className='mb-0'>Web Developer</h3>
+                <div className='subheading mb-3'>AppRight Software Solutions</div>
+                <p className='about'> It's a Agricultural website...</p>
+                <div className='resume-date text-md-right'><span className='text-primary'> June 2021 - Present</span></div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className='resume-section p-3 p-lg-5' id='education'>
+          <div className='auto'>
+          <h2 className='mb-5'>Education</h2>
+          <div className='resume-item'>
+            <div className='resume-content mr-auto'>
+                <h3 className='mb-0'>VIJAYANAGARA SRI KRISHNADEVARAYA UNIVERSITY</h3>
+                <div className='subheading mb-3'>Bachelor of Business Administration</div>
+                <div className='about'>Human Resource</div>
+                <p className='about'>GPA:</p>
+            </div>
+            <div className='resume-date'>
+                <span className='text-primary'> June 2016 - May 2019</span>
+            </div>
+          </div>
           </div>
         </section>
       </div>
@@ -29,9 +58,3 @@ function App() {
 }
 
 export default App;
-
-//  <span class="fa-stack">
-//                   <i class="fa fa-circle fa-stack-2x"></i>
-//                   <i class="fa fa-facebook" aria-hidden="true"></i>
-//                   <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
-//                 </span> 
